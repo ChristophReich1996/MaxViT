ac33168a2e1a2863628047b437a093bd8f13b371
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2022-04-07 13:56:53 +0200

    Implement Relative Self-Attention layer

--- maxvit/maxvit.py
+++ maxvit/maxvit.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from timm.models.efficientnet_blocks import SqueezeExcite, DepthwiseSeparableConv
-from timm.models.layers import drop_path
+from timm.models.layers import drop_path, trunc_normal_
 
 
 def _gelu_ignore_parameters(
@@ -25,7 +25,7 @@ def _gelu_ignore_parameters(
 
 
 class MBConv(nn.Module):
-    """ MBConv block as described in: https://arxiv.org/pdf/2204.01697.pdf
+    """ MBConv block as described in: https://arxiv.org/pdf/2204.01697.pdf.
 
         Without downsampling:
         x ← x + Proj(SE(DWConv(Conv(Norm(x)))))
@@ -42,10 +42,10 @@ class MBConv(nn.Module):
     Args:
             in_channels (int): Number of input channels.
             out_channels (int): Number of output channels.
-            downscale (bool): If true downscale by a factor of two is performed. Default: False
-            act_layer (Type[nn.Module]): Type of activation layer to be utilized. Default: nn.GELU
-            norm_layer (Type[nn.Module]): Type of normalization layer to be utilized. Default: nn.BatchNorm
-            drop_path_rate (float): Dropout rate to be applied during training. Default 0.
+            downscale (bool, optional): If true downscale by a factor of two is performed. Default: False
+            act_layer (Type[nn.Module], optional): Type of activation layer to be utilized. Default: nn.GELU
+            norm_layer (Type[nn.Module], optional): Type of normalization layer to be utilized. Default: nn.BatchNorm
+            drop_path_rate (float, optional): Dropout rate to be applied during training. Default 0.
     """
 
     def __init__(
@@ -110,7 +110,7 @@ def window_partition(
 
     Args:
         input (torch.Tensor): Input tensor of the shape [B, C, H, W].
-        window_size (Tuple[int, int]): Window size to be applied.
+        window_size (Tuple[int, int], optional): Window size to be applied. Default (7, 7).
 
     Returns:
         windows (torch.Tensor): Unfolded input tensor of the shape [B * windows, window_size[0], window_size[1], C].
@@ -134,10 +134,10 @@ def window_reverse(
     Args:
         windows (torch.Tensor): Window tensor of the shape [B * windows, window_size[0], window_size[1], C].
         original_size (Tuple[int, int]): Original shape.
-        window_size (Tuple[int, int]): Window size to be applied.
+        window_size (Tuple[int, int], optional): Window size which have been applied. Default (7, 7)
 
     Returns:
-        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]]
+        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]].
     """
     # Get height and width
     H, W = original_size
@@ -157,7 +157,7 @@ def grid_partition(
 
     Args:
         input (torch.Tensor): Input tensor of the shape [B, C, H, W].
-        grid_size (Tuple[int, int]): Grid size to be applied.
+        grid_size (Tuple[int, int], optional): Grid size to be applied. Default (7, 7).
 
     Returns:
         grid (torch.Tensor): Unfolded input tensor of the shape [B * grids, grid_size[0], grid_size[1], C].
@@ -181,10 +181,10 @@ def grid_reverse(
     Args:
         Grid (torch.Tensor): Grid tensor of the shape [B * grids, grid_size[0], grid_size[1], C].
         original_size (Tuple[int, int]): Original shape.
-        grid_size (Tuple[int, int]): Grid size to be applied.
+        grid_size (Tuple[int, int], optional): Grid size which have been applied. Default (7, 7).
 
     Returns:
-        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]]
+        output (torch.Tensor): Folded output tensor of the shape [B, C, original_size[0], original_size[1]].
     """
     # Get height, width, and channels
     (H, W), C = original_size, grid.shape[-1]
@@ -196,7 +196,42 @@ def grid_reverse(
     return output
 
 
-class SelfAttention(nn.Module):
+def get_relative_position_index(
+        win_h: int,
+        win_w: int
+) -> torch.Tensor:
+    """ Function to generate pair-wise relative position index for each token inside the window.
+        Taken from Timms Swin V1 implementation.
+
+    Args:
+        win_h (int): Window/Grid height.
+        win_w (int): Window/Grid width.
+
+    Returns:
+        relative_coords (torch.Tensor): Pair-wise relative position indexes [height * width, height * width].
+    """
+    coords = torch.stack(torch.meshgrid([torch.arange(win_h), torch.arange(win_w)]))
+    coords_flatten = torch.flatten(coords, 1)
+    relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+    relative_coords = relative_coords.permute(1, 2, 0).contiguous()
+    relative_coords[:, :, 0] += win_h - 1
+    relative_coords[:, :, 1] += win_w - 1
+    relative_coords[:, :, 0] *= 2 * win_w - 1
+    return relative_coords.sum(-1)
+
+
+class RelativeSelfAttention(nn.Module):
+    """ Relative Self-Attention similar to Swin V1. Implementation inspired by Timms Swin V1 implementation.
+
+    Args:
+        in_channels (int): Number of input channels
+        partition_function (Callable): Partition function (grid or block).
+        reverse_function (Callable): Function to reverse partition function (grid or block).
+        num_heads (int, optional): Number of attention heads. Default 32.
+        grid_window_size (Tuple[int, int], optional): Grid/Window size to be utilized. Default (7, 7).
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+        proj_drop (float, optional): Dropout ratio of output. Default: 0.0
+    """
 
     def __init__(
             self,
@@ -204,9 +239,83 @@ class SelfAttention(nn.Module):
             partition_function: Callable,
             reverse_function: Callable,
             num_heads: int = 32,
+            grid_window_size: Tuple[int, int] = (7, 7),
+            attn_drop: float = 0.,
+            proj_drop: float = 0.
     ) -> None:
         # Call super constructor
-        super(SelfAttention, self).__init__()
+        super(RelativeSelfAttention, self).__init__()
+        # Save parameters
+        self.in_channels: int = in_channels
+        self.partition_function: Callable = partition_function
+        self.reverse_function: Callable = reverse_function
+        self.num_heads: int = num_heads
+        self.grid_window_size: Tuple[int, int] = grid_window_size
+        self.scale: float = num_heads ** -0.5
+        self.attn_area: int = grid_window_size[0] * grid_window_size[1]
+        # Init layers
+        self.qkv_mapping = nn.Linear(in_features=in_channels, out_features=3 * in_channels, bias=True)
+        self.attn_drop = nn.Dropout(p=attn_drop)
+        self.proj = nn.Linear(in_features=in_channels, out_features=in_channels, bias=True)
+        self.proj_drop = nn.Dropout(p=proj_drop)
+        self.softmax = nn.Softmax(dim=-1)
+        # Define a parameter table of relative position bias, shape: 2*Wh-1 * 2*Ww-1, nH
+        self.relative_position_bias_table = nn.Parameter(
+            torch.zeros((2 * grid_window_size[0] - 1) * (2 * grid_window_size[1] - 1), num_heads))
+
+        # Get pair-wise relative position index for each token inside the window
+        self.register_buffer("relative_position_index", get_relative_position_index(grid_window_size[0],
+                                                                                    grid_window_size[1]))
+        # Init relative positional bias
+        trunc_normal_(self.relative_position_bias_table, std=.02)
+
+    def _get_relative_positional_bias(
+            self
+    ) -> torch.Tensor:
+        """ Returns the relative positional bias.
+
+        Returns:
+            relative_position_bias (torch.Tensor): Relative positional bias.
+        """
+        relative_position_bias = self.relative_position_bias_table[
+            self.relative_position_index.view(-1)].view(self.attn_area, self.attn_area, -1)
+        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()
+        return relative_position_bias.unsqueeze(0)
+
+    def forward(
+            self,
+            input: torch.Tensor
+    ) -> torch.Tensor:
+        """ Forward pass.
+
+        Args:
+            input (torch.Tensor): Input tensor of the shape [B, C, H, W].
+
+        Returns:
+            output (torch.Tensor): Output tensor of the shape [B, C, H, W].
+        """
+        # Save original shape
+        B, C, H, W = input.shape
+        # Perform partition
+        input_partitioned = self.partition_function(input, self.grid_window_size)
+        input_partitioned = input_partitioned.view(-1, self.grid_window_size[0] * self.grid_window_size[1], C)
+        # Get shape of partitioned input
+        B_, N = input_partitioned.shape[:2]
+        # Perform query key value mapping
+        qkv = self.qkv_mapping(input_partitioned).reshape(B_, N, 3, self.num_heads, -1).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+        # Scale query
+        q = q * self.scale
+        # Compute attention maps
+        attn = self.softmax(q @ k.transpose(-2, -1) + self._get_relative_positional_bias())
+        # Map value with attention maps
+        output = (attn @ v).transpose(1, 2).reshape(B_, N, -1)
+        # Perform final projection and dropout
+        output = self.proj(output)
+        output = self.proj_drop(output)
+        # Reverse partition
+        output = self.reverse_function(output, (H, W), self.grid_window_size)
+        return output
 
 
 class MaxViTBlock(nn.Module):
@@ -254,10 +363,22 @@ class MaxViT(nn.Module):
 
 
 if __name__ == '__main__':
-    input = torch.rand(7, 3, 14, 14)
-    windows = window_partition(input=input)
-    windows = window_reverse(windows=windows, window_size=(7, 7), original_size=input.shape[2:])
-    print(torch.all(input == windows))
-    grid = grid_partition(input=input)
-    grid = grid_reverse(grid=grid, grid_size=(7, 7), original_size=input.shape[2:])
-    print(torch.all(input == grid))
+    def test_partition_and_revers() -> None:
+        input = torch.rand(7, 3, 14, 14)
+        windows = window_partition(input=input)
+        windows = window_reverse(windows=windows, window_size=(7, 7), original_size=input.shape[2:])
+        print(torch.all(input == windows))
+        grid = grid_partition(input=input)
+        grid = grid_reverse(grid=grid, grid_size=(7, 7), original_size=input.shape[2:])
+        print(torch.all(input == grid))
+
+
+    def test_relative_self_attention() -> None:
+        relative_self_attention = RelativeSelfAttention(in_channels=128, partition_function=grid_partition,
+                                                        reverse_function=grid_reverse)
+        input = torch.rand(4, 128, 14, 14)
+        output = relative_self_attention(input)
+        print(output.shape)
+
+
+    test_relative_self_attention()
